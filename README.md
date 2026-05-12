4a496bac4ab7bec22fd01ff8cfb9a1c0a028892c
ajsandovalt <antonio.sandoval@posteo.net>
2026-05-11 18:56:33 -0600

    Completed write file feature

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Codeslope
 AI agent to assist in Coding
+
+
+## ⛔ !!! WARNING !!!!
+
+### This project doesn't have all the security and safety features that a production AI agent would have. This is for learning purposes only.
--- functions/write_file.py
+++ functions/write_file.py
@@ -10,11 +10,15 @@ def write_file(working_directory, file_path, content):
         if not valid_target_file:
             return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
         
-        if not os.path.isfile(target_file):
-            return f'Error: File not found or is not a regular file: "{file_path}"'
-        
         if os.path.isdir(target_file):
             return f'Error: Cannot write to "{file_path}" as it is a directory'
+        
+        os.makedirs(os.path.dirname(target_file), exist_ok=True)
+        
+        with open(target_file, mode='w') as file:
+            file.write(content)
+        
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
 
     except Exception as e:
         return f"Error: An exception has occurred: {e}"
